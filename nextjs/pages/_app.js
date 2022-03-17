44cd32b7305bb85046f432694f115243e007b070
Jacques Blom <me@jacquesblom.com>
2022-03-17 20:50:14 +0200

    Token

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,26 +1,32 @@
-import Script from 'next/script'
-import Layout from '@/components/Layout'
-import SEO from '@/components/SEO'
-import '@/styles/globals.css'
-import {QueryClient, QueryClientProvider} from 'react-query'
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Fudge from '@fudge-ai/nextjs'
+import Script from "next/script"
+import Layout from "@/components/Layout"
+import SEO from "@/components/SEO"
+import "@/styles/globals.css"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import Fudge from "@fudge-ai/nextjs"
 
-Fudge.init('test8394-1627-44c9-848f-38971fcaef34')
+if (!process.env.NEXT_PUBLIC_FUDGE_KEY) {
+    throw new Error(
+        "Missing NEXT_PUBLIC_FUDGE_KEY environment variable. Please sign up for an account at app.fudge.ai to get your token."
+    )
+}
+
+Fudge.init(process.env.NEXT_PUBLIC_FUDGE_KEY)
 
 const queryClient = new QueryClient()
 
-function MyApp({Component, pageProps}) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Layout>
-        <SEO title={process.env.siteTitle} />
-        <Component {...pageProps} />
-        <ToastContainer theme="colored" />
-      </Layout>
-    </QueryClientProvider>
-  )
+function MyApp({ Component, pageProps }) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Layout>
+                <SEO title={process.env.siteTitle} />
+                <Component {...pageProps} />
+                <ToastContainer theme="colored" />
+            </Layout>
+        </QueryClientProvider>
+    )
 }
 
 export default MyApp
