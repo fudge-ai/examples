dc60c82af2855ddd4c71252a3ed82f7ab68743f9
Jacques Blom <me@jacquesblom.com>
2022-03-17 20:51:05 +0200

    Error

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -7,17 +7,25 @@ import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import Fudge from "@fudge-ai/nextjs"
 
-if (!process.env.NEXT_PUBLIC_FUDGE_KEY) {
-    throw new Error(
-        "Missing NEXT_PUBLIC_FUDGE_KEY environment variable. Please sign up for an account at app.fudge.ai to get your token."
-    )
+if (process.env.NEXT_PUBLIC_FUDGE_KEY) {
+    Fudge.init(process.env.NEXT_PUBLIC_FUDGE_KEY)
 }
-
-Fudge.init(process.env.NEXT_PUBLIC_FUDGE_KEY)
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
+    if (!process.env.NEXT_PUBLIC_FUDGE_KEY) {
+        return (
+            <div>
+                <div>Missing NEXT_PUBLIC_FUDGE_KEY environment variable.</div>
+                <div>
+                    Please sign up for an account at app.fudge.ai to get your
+                    token.
+                </div>
+            </div>
+        )
+    }
+
     return (
         <QueryClientProvider client={queryClient}>
             <Layout>
